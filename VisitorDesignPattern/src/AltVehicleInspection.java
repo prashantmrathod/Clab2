ab45804c6ef48c69fca1858fb19baf92ff926c99
prash <prashanthrathod32@gmail.com>
2025-03-30 22:51:08 -0700

    Implemented alternate inspection logic

--- VisitorDesignPattern/src/AltVehicleInspection.java
+++ VisitorDesignPattern/src/AltVehicleInspection.java
@@ -1,37 +1,40 @@
 public class AltVehicleInspection implements IVehicleInspector {
     @Override
     public int visit(Car car) {
+
         int serviceCharge = 0;
-        if(car.getColor()=="Black"){
-            serviceCharge += 100;
-        }else{
-            serviceCharge += 50;
+        if (car.getColor().equalsIgnoreCase("Red")) {
+            serviceCharge += 150;
+        } else {
+            serviceCharge += 75;
         }
-        System.out.println("Service Charge for Car: " + serviceCharge);
+        System.out.println("Alt Service Charge for Car: " + serviceCharge);
         return serviceCharge;
     }
 
     @Override
     public int visit(Van van) {
+
         int serviceCharge = 0;
-        if(van.getNumberOfDoors()>4){
-            serviceCharge += 500;
-        }else{
-            serviceCharge += 100;
+        if (van.getNumberOfDoors() > 3) {
+            serviceCharge += 400;
+        } else {
+            serviceCharge += 200;
         }
-        System.out.println("Service Charge for Van: " + serviceCharge);
+        System.out.println("Alt Service Charge for Van: " + serviceCharge);
         return serviceCharge;
     }
 
     @Override
     public int visit(Motorbike motorbike) {
+
         int serviceCharge = 0;
-        if(motorbike.getEngineCapacity()>=200){
-            serviceCharge += 200;
-        }else{
-            serviceCharge += 50;
+        if (motorbike.getEngineCapacity() >= 150) {
+            serviceCharge += 250;
+        } else {
+            serviceCharge += 100;
         }
-        System.out.println("Service Charge for Motorbike: " + serviceCharge);
+        System.out.println("Alt Service Charge for Motorbike: " + serviceCharge);
         return serviceCharge;
     }
 }
